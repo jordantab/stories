d7a2c1f3145fa37f9daf696f04b5d219a4fb7635
jordantab <jordantab20@gmail.com>
2024-04-27 11:37:07 -0700

    Established mongodb connection

--- server/server.py
+++ server/server.py
@@ -1,12 +1,27 @@
 from flask import Flask
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
+    uri = "mongodb+srv://temp_user:1234@stories.detzj4q.mongodb.net/?retryWrites=true&w=majority&appName=Stories"
+    client = MongoClient(uri, server_api=ServerApi('1'))
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+    with app.app_context():
+        # Try to connect and ping MongoDB when the app starts
+        try:
+            client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+    
+    @app.route('/')
+    def home():
+        return 'Hello, World!'
 
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
