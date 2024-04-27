cc56f4edc0ca2d7bcd4cd3338a61c03a0b61a794
Jack Michalak <jack@halloapp.com>
2024-04-27 12:40:34 -0700

    Start on story interface

--- web/src/Story.tsx
+++ web/src/Story.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+
+
+import React, { useState } from 'react';
 
 
 function Story() {
   return (
-    <div>
-      Story
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-row">Progress</div>
+      <div className="flex-grow">Content</div>
+      <div className="">Paging</div>
     </div>
   );
 }
