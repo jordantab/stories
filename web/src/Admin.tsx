af8af7b2c651fe98e63b8999aabc124ea0497523
Jack Michalak <jack@halloapp.com>
2024-04-27 12:14:24 -0700

    Basic layout of admin interface

---
+++ web/src/Admin.tsx
@@ -0,0 +1,57 @@
+
+
+import React, { useState } from 'react';
+
+import { MdOutlineShare } from "react-icons/md";
+import { HiOutlineTrash } from "react-icons/hi";
+import { LuDownload } from "react-icons/lu";
+
+interface Story {
+  title: string,
+  subtitle: string,
+}
+
+const DEFAULT_ITEMS = [
+  {
+    title: "The Big Book of Generative AI",
+    subtitle: "White Paper",
+  },
+  {
+    title: "MLOps for Dummies",
+    subtitle: "E-Book",
+  },
+]
+
+function Admin() {
+  const [items, setItems] = useState<Story[]>(DEFAULT_ITEMS)
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="p-4 flex flex-row justify-between items-center border-b">
+        <div className="text-purple-500 font-bold">stories</div>
+        <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-1 px-3 rounded">+ Create Story</button>
+      </div>
+      <div className="flex-1 py-10 px-20 space-y-4">
+        <h1 className="text-5xl">Your Stories</h1>
+        {items.map(item => story(item))}
+      </div>
+    </div>
+  );
+}
+
+function story(story: Story) {
+  return (
+    <div className="w-full border p-4 rounded flex flex-row justify-between items-center">
+      <div className="flex flex-col">
+        <div className="text-lg font-bold">{story.title}</div>
+        <div className="text-md">{story.subtitle}</div>
+      </div>
+      <div className="flex flex-row space-x-4">
+        <button className="border rounded p-2 flex flex-row space-x-2"><LuDownload size={24} /><div className="font-bold">Download leads</div></button>
+        <button className="border rounded p-2"><MdOutlineShare size={24} /></button>
+        <button className="border rounded p-2"><HiOutlineTrash size={24} /></button>
+      </div>
+    </div>
+  )
+}
+
+export default Admin;
---
+++ web/src/Story.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Story() {
+  return (
+    <div>
+      Story
+    </div>
+  );
+}
+
+export default Story;
