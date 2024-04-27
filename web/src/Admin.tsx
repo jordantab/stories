94f3dee0858a245a68402847e3c15a9944130aa2
Jack Michalak <jack@halloapp.com>
2024-04-27 12:38:47 -0700

    Make modal interactions

--- web/src/Admin.tsx
+++ web/src/Admin.tsx
@@ -28,11 +28,48 @@ const DEFAULT_ITEMS = [
 function Admin() {
   const [items, setItems] = useState<Story[]>(DEFAULT_ITEMS)
   const [showModal, setShowModal] = useState(true)
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: (acceptedFiles) => {
+      console.log(acceptedFiles);
+    }
+  });
+
+  function Modal() {
+    return (
+      <div className="fixed inset-0 bg-gray-600 bg-opacity-50 z-50 flex justify-center items-center">
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+            <h2 className="text-3xl font-bold mb-4">New Story</h2>
+            <p className="mb-4 text-gray-500">Story will be automatically generated with GenAI from any of your existing white papers or docs</p>
+            <p className="text-sm">Upload your pdf</p>
+            <div {...getRootProps({ className: 'dropzone border border-gray-300 rounded-lg p-10 text-center' })}>
+                <input {...getInputProps()} />
+                <div className="flex flex-row justify-center items-center space-x-2"><MdOutlineFileUpload size={18} /><p>Drop files here</p></div>
+              </div>
+            <div className="flex flex-row justify-end pt-4 space-x-4">
+              <button className="bg-gray-100 hover:bg-gray-300 border font-bold py-2 px-4 rounded"
+              onClick={() => setShowModal(false)}
+              >
+                  Cancel
+              </button>
+              <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => setShowModal(false)}
+              >
+                  Create
+              </button>
+            </div>
+        </div>
+    </div>
+    )
+  }
+
   return (
     <div className="flex flex-col h-screen">
       <div className="p-4 flex flex-row justify-between items-center border-b">
         <div className="text-purple-500 font-bold">stories</div>
-        <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-1 px-3 rounded">+ Create Story</button>
+        <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-1 px-3 rounded"
+          onClick={() => setShowModal(true)}
+        >+ Create Story</button>
       </div>
       <div className="flex-1 py-10 px-20 space-y-4">
         <h1 className="text-5xl">Your Stories</h1>
@@ -56,35 +93,6 @@ function story(story: Story) {
         <button className="border rounded p-2"><HiOutlineTrash size={24} /></button>
       </div>
     </div>
-  )
-}
-
-function Modal() {
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop: (acceptedFiles) => {
-      console.log(acceptedFiles);
-    }
-  });
-  return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 z-50 flex justify-center items-center">
-      <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h2 className="text-3xl font-bold mb-4">New Story</h2>
-          <p className="mb-4 text-gray-500">Story will be automatically generated with GenAI from any of your existing white papers or docs</p>
-          <p className="text-sm">Upload your pdf</p>
-          <div {...getRootProps({ className: 'dropzone border border-gray-300 rounded-lg p-10 text-center' })}>
-              <input {...getInputProps()} />
-              <div className="flex flex-row justify-center items-center space-x-2"><MdOutlineFileUpload size={18} /><p>Drop files here</p></div>
-            </div>
-          <div className="flex flex-row justify-end">
-            <button className="bg-gray-100 hover:bg-gray-300 border font-bold py-2 px-4 rounded">
-                Cancel
-            </button>
-            <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
-                Create
-            </button>
-          </div>
-      </div>
-  </div>
   )
 }
 
