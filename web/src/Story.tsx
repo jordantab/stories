617adaa3aee56794eb884b0a82a4588bdf50110d
Jack Michalak <jack@halloapp.com>
2024-04-27 14:52:47 -0700

    Test out the query page

--- web/src/Story.tsx
+++ web/src/Story.tsx
@@ -21,9 +21,26 @@ function makeBar(dark: boolean) {
   return <div className={"h-1 w-12 rounded " + (dark ? "bg-black" : "bg-gray-300")}></div>
 }
 
+const TEST_PAGE_1: Page = {
+  number: 0,
+  title: "Google Cloud",
+  text: "Data Warehousing in the Age of AI",
+  type: "display",
+  img_url: "",
+}
+
+const TEST_PAGE_2: Page = {
+  number: 0,
+  title: "",
+  text: "Let's start with your email",
+  type: "query",
+  img_url: "",
+}
+
 function StoryPage() {
   const [index, setIndex] = useState(0)
-  const [page, setPage] = useState<Page|undefined>(undefined)
+  const [page, setPage] = useState<Page|undefined>(TEST_PAGE_2)
+  const [queryResponse, setQueryResponse] = useState("")
 
   const { story_id } = useParams();
   console.log("Loading for story", story_id)
@@ -66,8 +83,22 @@ function StoryPage() {
         })}
       </div>
       <div className="flex-grow flex flex-col justify-center space-y-4 py-8 px-20">
-        <h2 className="text-6xl font-bold">{page.title}</h2>
-        <p className="text-2xl">{page.text}</p>
+        {
+          page.type === "query" ? (
+            <>
+              <p className="text-2xl font-bold">{page.text}</p>
+              <input value={queryResponse}
+                onChange={ev => setQueryResponse(ev.target.value)} className="border rounded-lg p-3 w-1/4"
+                type="text" placeholder="Type your answer here" />
+            </>
+          ) : (
+            <>
+              <h2 className="text-6xl font-bold">{page.title}</h2>
+              <p className="text-2xl">{page.text}</p>
+            </>
+          )
+        }
+
       </div>
       <div className="flex flex-row justify-between p-8">
         <IoArrowBackCircle size={arrowSize} onClick={() => prevPage()} />
