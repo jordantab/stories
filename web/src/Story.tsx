8f74a7d26e6138e82928d3de1e75c626a46551e5
Jack Michalak <jack@halloapp.com>
2024-04-27 13:24:07 -0700

    Create the progress indicators

--- web/src/Story.tsx
+++ web/src/Story.tsx
@@ -2,11 +2,23 @@
 
 import React, { useState } from 'react';
 
+function nums(size: number) {
+  return new Array(size).fill(null).map((_, i) => i)
+}
+
+function makeBar(dark: boolean) {
+  return <div className={"h-1 w-12 rounded " + (dark ? "bg-black" : "bg-gray-300")}></div>
+}
 
 function Story() {
+  const [page, setPage] = useState(0)
   return (
     <div className="h-screen flex flex-col">
-      <div className="flex flex-row">Progress</div>
+      <div className="flex flex-row justify-around px-20 py-4">
+        {nums(10).map(i => {
+          return (<div key={i}>{makeBar(i <= page)}</div>)
+        })}
+      </div>
       <div className="flex-grow">Content</div>
       <div className="">Paging</div>
     </div>
